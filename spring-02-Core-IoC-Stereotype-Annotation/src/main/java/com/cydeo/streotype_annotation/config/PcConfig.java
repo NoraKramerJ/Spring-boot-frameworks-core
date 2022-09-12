11ff27d6f6ec93f2d54de7b95e80326671332ea0
Nmerkel <noramerkel0@gmail.com>
2022-09-12 15:06:02 -0400

    learning wiring

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/cydeo/streotype_annotation/config/PcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.cydeo.streotype_annotation")
+// this is when to tell where to scan for the class that contains the @Component
 public class PcConfig {
 
 }
