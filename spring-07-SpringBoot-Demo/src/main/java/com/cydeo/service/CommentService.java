95cf17e2bcc538a10b97866c32d2ea0dd89bdc98
Nmerkel <noramerkel0@gmail.com>
2022-09-15 15:59:14 -0400

    Spring-Boot Demo

---
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,40 @@
+package com.cydeo.service;
+
+import com.cydeo.config.AppConfigData;
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+    private final AppConfigData appConfigData;
+
+    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, AppConfigData appConfigData) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+        this.appConfigData = appConfigData;
+    }
+
+    public void publishComment(Comment comment){
+
+     commentRepository.storeComment(comment);
+     commentNotificationProxy.sendComment(comment);
+        //save in the database
+        //send email
+    }
+
+    public void printConfigData(){
+        System.out.println(appConfigData.getUserName());
+        System.out.println(appConfigData.getPassword());
+        System.out.println(appConfigData.getUrl());
+        //print Ozzy
+        //PRINT ABC123
+        //PRINT URL
+    }
+}
