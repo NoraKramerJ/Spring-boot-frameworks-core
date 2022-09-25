37b130ee1176d6151046d4dbfac5d05cf7c3bc67
Nmerkel <noramerkel0@gmail.com>
2022-09-25 12:27:28 -0400

    learning @RequestParam

---
+++ spring-10--ThymeLeaf/src/main/java/com/cydeo/Spring10ThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring10thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10ThymeLeafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring10ThymeLeafApplication.class, args);
+    }
+
+}
