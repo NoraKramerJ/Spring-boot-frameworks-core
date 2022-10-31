605d95e3e585a7ea90c0a27b3a624a76d2dedf0e
Nmerkel <noramerkel0@gmail.com>
2022-10-31 16:53:03 -0400

    learning about queries

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Ticket.java
@@ -1,7 +1,8 @@
 package com.cydeo.entity;
 
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,7 +11,8 @@ import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class Ticket extends BaseEntity {
 
@@ -23,6 +25,13 @@ public class Ticket extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private User userAccount;
 
-
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "seatNumber=" + seatNumber +
+                ", rowNumber=" + rowNumber +
+                ", dateTime=" + dateTime +
+                '}';
+    }
 
 }
