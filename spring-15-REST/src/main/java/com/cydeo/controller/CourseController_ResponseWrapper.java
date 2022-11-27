0445018a5b91d8c4be4d24aad0761c5323e29e5b
Nmerkel <noramerkel0@gmail.com>
2022-11-27 15:54:36 -0500

    CourseController_ResponseWrapper

--- spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseWrapper.java
@@ -1,11 +1,13 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.dto.CourseDTO;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.service.CourseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,11 @@ public class CourseController_ResponseWrapper {
         return ResponseEntity.status(HttpStatus.ACCEPTED)
                 .header("Version","Cydeo.V3")
                 .body(new ResponseWrapper("courses successfully retrieved", courseService.getCourses()));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ResponseWrapper> getCourseById(@PathVariable("id") long courseId){
+        return ResponseEntity.ok(new ResponseWrapper("course:" + courseId + "retrieved",courseService.getCourseById(courseId)));
+
     }
 }
