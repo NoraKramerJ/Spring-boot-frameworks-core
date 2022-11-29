89957f3d36c30dd1199cb5d27fb235204059de40
Nmerkel <noramerkel0@gmail.com>
2022-11-29 14:41:21 -0500

    Created DTOS then to consume the API

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/controller/SchoolController.java
@@ -10,9 +10,7 @@ import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -58,5 +56,11 @@ public class SchoolController {
         AddressDTO addressDTO=addressService.findById(id);
         return ResponseEntity.ok(new ResponseWrapper("Address is successfully retrieved",addressDTO));
 
+    }
+    @PutMapping("/address/{id}")
+    public AddressDTO updateAddress(@PathVariable("id") long id, @RequestBody AddressDTO addressDTO) throws Exception {
+
+        addressDTO.setId(id);
+       return addressService.update(addressDTO);
     }
 }
